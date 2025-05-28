a71b62c1b51ba798bc4f7389b267fe6a1695b5f1
Documenter.jl <documenter@juliadocs.github.io>
2025-05-28 10:29:01 +0000

    build based on d52f92b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FibonacciChain","page":"Home","title":"FibonacciChain","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FibonacciChain.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
