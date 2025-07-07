f408d8d8289bea16c1e6d9d3352150611a54ef08
Zhaohui Zhi <2276780832@qq.com>
2025-07-07 21:40:32 +0800

    edit para

--- exm/Bulk_measure/monitored_dynamics.jl
+++ exm/Bulk_measure/monitored_dynamics.jl
@@ -1,11 +1,9 @@
 using FibonacciChain
-using Plots
-using LaTeXStrings
 using JLD
 using Statistics
 # include("../FitEntEntScal.jl")
 
-function samples_generate(L::Int64, τ::Float64, D::Int64=100L)
+function samples_generate(L::Int64, τ::Float64, D::Int64=30L)
     st=zeros(length(Fibonacci_basis(L)))
     st[1] = 1.0
     samples_num = 2000
@@ -25,7 +23,7 @@ function samples_generate(L::Int64, τ::Float64, D::Int64=100L)
     # return sample_measured_states, samples, sample_weights
 end
 
-function monitored_dynamics(L::Int64, τ::Float64, D::Int64=500L)
+function monitored_dynamics(L::Int64, τ::Float64, D::Int64=30L)
     st=zeros(length(Fibonacci_basis(L)))
     st[1] = 1.0
     average_EElis=zeros(L-1)
@@ -64,7 +62,7 @@ function monitored_dynamics(L::Int64, τ::Float64, D::Int64=500L)
            average_FE_tlis, stderr_FE_tlis, average_FE, stderr_FE
 end
 
-function sample_calculate(L::Int64, τ::Float64, D::Int64=100L)
+function sample_calculate(L::Int64, τ::Float64, D::Int64=30L)
     st=zeros(length(Fibonacci_basis(L)))
     st[1] = 1.0
     average_EElis=zeros(L-1)
@@ -98,35 +96,18 @@ function sample_calculate(L::Int64, τ::Float64, D::Int64=100L)
     stderr_FE_tlis = (std(all_FE_tlis, dims=1) ./ sqrt(samples_num))[:]
 
     
-    save("exm/data/Bulk_measure/monitored_EE_FEdynamics_L$(L)_τ$(τ)_D$(D).jld", "average_EE_tlis", average_EE_tlis, "stderr_EE_tlis", stderr_EE_tlis, "average_EElis", average_EElis, "stderr_EElis", stderr_EElis, 
+    save("exm/data/Bulk_measure/monitored_EE_FEdynamics_L$(L)_τ$(τ)_D$(div(D,L)).jld", "average_EE_tlis", average_EE_tlis, "stderr_EE_tlis", stderr_EE_tlis, "average_EElis", average_EElis, "stderr_EElis", stderr_EElis, 
     "average_FE", average_FE, "stderr_FE", stderr_FE, 
     "average_FE_tlis", average_FE_tlis, "stderr_FE_tlis", stderr_FE_tlis)
 end
 
-# L = 10
-# τ = 0.1
-# D = 60L
-# average_EElis, EE_tlis = post_selection(L, τ, D, :m)
-# fig1=plot(range(0, D/2, D), EE_tlis, label=false, xlabel=L"t", ylabel=L"S_{vN}", title="Post-selection 0")
-# # plot!([1, 30*10], 0.8933161189003952*[1,1],  c=:Gray, label=false, linestyle=:dash, linewidth=2)
-# cent, fig = fitCCEntEntScal(average_EElis, mincut=2, pbc=true)
-# display(fig)
-# save("exm/data/monitored_dynamics.jld", "cent", cent, "fig", fig)
-# save("exm/data/monitored_dynamics.jld", "average_EElis", average_EElis, "EE_tlis", EE_tlis)
-# savefig(fig, "/Users/cycling/Documents/projects/NoisyFibonacciChain/figs/Bulk_measure/monitored_dynamics_N28_D15_eescaling.pdf")
-# savefig(fig1, "/Users/cycling/Documents/projects/NoisyFibonacciChain/figs/Bulk_measure/monitored_dynamics_N28_D15_S.pdf")
 ## == monitored_dynamics == ##
-L = 10
+# L = 10
 τ = log(1+√2)
-D = 100L
-inds = collect(1:10:D)
-# inds = vcat(collect())
-# inds = collect(1:6000)
-all_EE_tlis, average_EE_tlis, stderr_EE_tlis, all_EElis, average_EElis, stderr_EElis = monitored_dynamics(L, τ, D)
-# fig_monitored_N8=plot(range(0, D/2, D)[inds], average_EE_tlis[:][inds], yerror=stderr_EE_tlis[inds], label=false, xaxis=:log, xlabel=L"t", ylabel=L"S_{vN}", title=latexstring("N=$(L), τ=$(τ)"))
-# fit_samples = scatter(range(0, D/2, D)[inds], all_EE_tlis'[inds,:], xlabel=L"t", ylabel=L"S_{vN}", legend = false, colors= cgrad(:blues, length(collect(1:100))), marker_z = collect(1:100)',  colorbar=false,  line_z = collect(1:100)', title=latexstring("N=$(L), τ=$(τ)"))
-# cent, fig = fitCCEntEntScal(average_EElis, mincut=1, pbc=true)
-# display(fig)
+# D = 100L
+
+# all_EE_tlis, average_EE_tlis, stderr_EE_tlis, all_EElis, average_EElis, stderr_EElis = monitored_dynamics(L, τ, D)
+
 
 if length(ARGS) == 0
     println("No arguments provided.")
