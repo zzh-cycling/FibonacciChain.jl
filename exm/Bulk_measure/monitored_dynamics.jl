d680a2bfaac8ca44a9442cb1225c533be492ef4b
Zhaohui Zhi <2276780832@qq.com>
2025-07-14 08:54:15 +0800

    new shell

--- exm/Bulk_measure/monitored_dynamics.jl
+++ exm/Bulk_measure/monitored_dynamics.jl
@@ -3,29 +3,29 @@ using JLD
 using Statistics
 using Random
 
-function samples_generate(L::Int64, τ::Float64, index::Int64, seed::Int64, D::Int64=35L)
+function samples_generate(L::Int64, τ::Float64, index::Int64, seed::Int64, D::Int64=120L)
     rng = MersenneTwister(seed)
     
     st = zeros(length(Fibonacci_basis(L)))
     st[1] = 1.0
     
-    @time sample_measured_states, sample, sample_free_energy = Bulkmeasure(L, τ, st, D, rng) 
+    @time sample_measured_states, sample, sample_free_energy = Bulkmeasure(L, τ, st, D, rng, true) 
     
     halfchain_EE_tlis = [eelis_Fibo_state(L, j)[div(L,2)] for j in sample_measured_states]
     final_state = sample_measured_states[end]
     final_EElis = eelis_Fibo_state(L, final_state)
 
     
-    save("./exm/data/Bulk_measure/Observable_monitored_dynamics/L$(L)/τ$(τ)_D$(div(D,L))_Samples$(index).jld", "halfchain_EE_tlis", halfchain_EE_tlis, "final_EElis ", final_EElis, "seed", seed, "sample_free_energy", sample_free_energy)
+    save("./exm/data/Bulk_measure/Observable_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(index).jld", "halfchain_EE_tlis", halfchain_EE_tlis, "final_EElis ", final_EElis, "seed", seed, "sample_free_energy", sample_free_energy)
 
-    save("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)_D$(div(D,L))_Samples$(index).jld", "sample", sample, "sample_free_energy", sample_free_energy, "seed", seed)
+    save("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(index).jld", "sample", sample, "sample_free_energy", sample_free_energy, "seed", seed)
     # return sample_measured_states, samples, sample_free_energy
 end
 
-function sample_continue_calculate(L::Int64, τ::Float64, index::Int64, seed::Int64, D::Int64=35L, additional_layers::Int64=15L)
+function sample_continue_calculate(L::Int64, τ::Float64, index::Int64, seed::Int64, D::Int64=120L, additional_layers::Int64=15L)
     rng = MersenneTwister(seed)
     
-    sample, sample_free_energy, seed= load("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)_D$(div(D,L))_Samples$(index).jld", "sample", "sample_free_energy","seed")
+    sample, sample_free_energy, seed= load("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(index).jld", "sample", "sample_free_energy","seed")
     st = Generate_state(τ, st, sample, true, true) 
     sample_measured_states, sample, sample_free_energy = Bulkmeasure(L, τ, st[end-1],additional_layers, rng) 
     halfchain_EE_tlis = [eelis_Fibo_state(L, j)[div(L,2)] for j in sample_measured_states]
@@ -33,19 +33,19 @@ function sample_continue_calculate(L::Int64, τ::Float64, index::Int64, seed::In
     final_EElis = eelis_Fibo_state(L, final_state)
 
     
-    save("./exm/data/Bulk_measure/Observable_monitored_dynamics/L$(L)/τ$(τ)_D$(div(D+additional_layers,L))_Samples$(index).jld", "halfchain_EE_tlis", halfchain_EE_tlis, "final_EElis ", final_EElis, "seed", seed, "sample_free_energy", sample_free_energy)
+    save("./exm/data/Bulk_measure/Observable_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D+additional_layers,L))_Samples$(index).jld", "halfchain_EE_tlis", halfchain_EE_tlis, "final_EElis ", final_EElis, "seed", seed, "sample_free_energy", sample_free_energy)
 
-    save("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)_D$(div(D+additional_layers,L))_Samples$(index).jld", "sample", sample, "sample_free_energy", sample_free_energy, "seed", seed)
+    save("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D+additional_layers,L))_Samples$(index).jld", "sample", sample, "sample_free_energy", sample_free_energy, "seed", seed)
 end
 
-function samples_collect(L::Int64, τ::Float64, D::Int64=35L)
+function samples_collect(L::Int64, τ::Float64, D::Int64=120L)
     samples_num = 2000
     ensemble = Vector{Matrix{Int}}(undef, samples_num)
     ensemble_free_energy = Vector{Vector{Float64}}(undef, samples_num)
     ensemble_seed = Vector{Int64}(undef, samples_num)
      for i in 1:samples_num
         @show i
-        @time sample, sample_free_energy, seed = load("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)_D$(div(D,L))_Samples$(i).jld", "sample", "sample_free_energy", "seed")
+        @time sample, sample_free_energy, seed = load("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(i).jld", "sample", "sample_free_energy", "seed")
         ensemble[i] = sample
         ensemble_free_energy[i] = sample_free_energy
         ensemble_seed[i] = seed
@@ -54,24 +54,8 @@ function samples_collect(L::Int64, τ::Float64, D::Int64=35L)
     save("exm/data/Bulk_measure/monitored_dynamics_ensemble_L$(L)_τ$(τ)_D$(div(D,L)).jld", "ensemble", ensemble, "ensemble_free_energy", ensemble_free_energy, "ensemble_seed", ensemble_seed)
 end
 
-function samples_calculate(L::Int64, τ::Float64, index::Int64, D::Int64=35L)
-    st=zeros(length(Fibonacci_basis(L)))
-    st[1] = 1.0
-    
-    sample, sample_free_energy, seed= load("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)_D$(div(D,L))_Samples$(index).jld", "sample", "sample_free_energy","seed")
 
-    # evolution by a measurement gate, Born's rule
-    sample_measured_states = Generate_state(τ, st, sample, true, true) 
-    # sample_free_energy = D-dimensional vector of (-ln conditional_P_of_each_layer) = free energy contributed by each layer of L/2 gates (PBC)
-    halfchain_EE_tlis = [eelis_Fibo_state(L, j)[div(L,2)] for j in sample_measured_states]
-    final_state = sample_measured_states[end]
-    final_EElis = eelis_Fibo_state(L, final_state)
-
-    
-    save("./exm/data/Bulk_measure/Observable_monitored_dynamics/L$(L)/τ$(τ)_D$(div(D,L))_Samples$(index).jld", "halfchain_EE_tlis", halfchain_EE_tlis, "final_EElis ", final_EElis, "seed", seed, "sample_free_energy", sample_free_energy)
-end
-
-function Observable_collect(L::Int64, τ::Float64, D::Int64=35L)
+function Observable_collect(L::Int64, τ::Float64, D::Int64=120L)
     samples_num = 2000
     ensemble_free_energy = Vector{Vector{Float64}}(undef, samples_num)
     ensemble_seed = Vector{Int64}(undef, samples_num)
@@ -80,7 +64,7 @@ function Observable_collect(L::Int64, τ::Float64, D::Int64=35L)
 
     for i in 1:samples_num
         @show i
-        halfchain_EE_tlis, final_EElis, seed, sample_free_energy = load("./exm/data/Bulk_measure/Observable_monitored_dynamics/L$(L)/τ$(τ)_D$(div(D,L))_Samples$(i).jld", "halfchain_EE_tlis", "final_EElis ", "seed",  "sample_free_energy")
+        halfchain_EE_tlis, final_EElis, seed, sample_free_energy = load("./exm/data/Bulk_measure/Observable_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(i).jld", "halfchain_EE_tlis", "final_EElis ", "seed",  "sample_free_energy")
 
         ensemble_EE_dynamics[i, :] = halfchain_EE_tlis
         ensemble_final_EElis[i, :] = final_EElis
@@ -97,40 +81,7 @@ function Observable_collect(L::Int64, τ::Float64, D::Int64=35L)
     save("exm/data/Bulk_measure/monitored_EE_FEdynamics_L$(L)_τ$(τ)_D$(div(D,L)).jld", "average_EE_tlis", average_EE_tlis, "stderr_EE_tlis", stderr_EE_tlis, "bulk_meanEElis", bulk_meanEElis, "ensemble_stderr_EElis",ensemble_stderr_EElis, "ensemble_free_energy", ensemble_free_energy, "ensemble_seed", ensemble_seed)
 end
 
-function ensemble_calculate(L::Int64, τ::Float64, D::Int64=35L)
-    st=zeros(length(Fibonacci_basis(L)))
-    st[1] = 1.0
-    bulk_meanEElis=zeros(L-1)
-    
-    samples_num = 2000
-
-    ensemble_EE_dynamics= zeros(samples_num, D) 
-    final_EElis = zeros(samples_num, L-1)
-
-
-    ensemble, ensemble_free_energy = load("./exm/data/Bulk_measure/monitored_dynamics_ensemble_L$(L)_τ$(τ)_D$(div(D,L)).jld", "ensemble", "ensemble_free_energy")
-
-    for i in 1:samples_num
-        @show i
-        # evolution by a measurement gate, Born's rule
-        sample_measured_states = Generate_state(τ, st, sample, true, true)  
-        # sample_free_energy = D-dimensional vector of (-ln conditional_P_of_each_layer) = free energy contributed by each layer of L/2 gates (PBC)
-
-        ensemble_EE_dynamics[i, :] = [eelis_Fibo_state(L, j)[div(L,2)] for j in sample_measured_states]
-        final_state = sample_measured_states[end]
-        final_EElis[i, :] = eelis_Fibo_state(L, final_state)
-    end
-
-    bulk_meanEElis = mean(final_EElis, dims=1)[:]
-    average_EE_tlis = mean(ensemble_EE_dynamics, dims=1)[:]
-    ensemble_stderr_EElis = (std(final_EElis, dims=1) ./ sqrt(samples_num))[:]
-    stderr_EE_tlis = (std(ensemble_EE_dynamics, dims=1) ./ sqrt(samples_num))[:]
-
-    
-    save("exm/data/Bulk_measure/monitored_EE_FEdynamics_L$(L)_τ$(τ)_D$(div(D,L)).jld", "average_EE_tlis", average_EE_tlis, "stderr_EE_tlis", stderr_EE_tlis, "bulk_meanEElis", bulk_meanEElis, "ensemble_stderr_EElis",ensemble_stderr_EElis, "ensemble_free_energy", ensemble_free_energy)
-end
-
-function monitored_dynamics(L::Int64, τ::Float64, D::Int64=35L)
+function monitored_dynamics(L::Int64, τ::Float64, D::Int64=120L)
     st=zeros(length(Fibonacci_basis(L)))
     st[1] = 1.0
     bulk_meanEElis=zeros(L-1)
@@ -169,15 +120,7 @@ function monitored_dynamics(L::Int64, τ::Float64, D::Int64=35L)
            average_FE_tlis, stderr_FE_tlis, average_FE, stderr_FE
 end
 
-
-
-## == monitored_dynamics == ##
-# L = 10
-τ = log(1+√2)
-# D = 100L
-
-# ensemble_EE_dynamics, average_EE_tlis, stderr_EE_tlis, all_EElis, bulk_meanEElis, ensemble_stderr_EElis = monitored_dynamics(L, τ, D)
-
+τ = atanh(0.3)
 
 if length(ARGS) == 0
     println("No arguments provided.")
@@ -187,7 +130,6 @@ else
     seed=parse(Int64, ARGS[3])
     println("Received argument: $N, $index")
     samples_generate(N, τ, index, seed)
-    # samples_calculate(N, τ, index)
     # Observable_collect(N, τ)
     # samples_collect(N, τ)
 end
--- exm/slurm_submit.sh
+++ exm/slurm_submit.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 #SBATCH -p i64m512u # u = shared nodes; ue = exclusive cores; 32 cores / node
-#SBATCH -J TMI20_index${arg}
+#SBATCH -J N20FibonaccidynamicsSamples
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=100
+#SBATCH --cpus-per-task=1
 #SBATCH --exclude=cpu1-1,cpu1-99,cpu1-78,cpu1-94,cpu1-101
 ## SBATCH -o job-%j-%a.txt  # 使用作业数组ID来区分输出文件
 #SBATCH -o job-%j.txt
 
 ##SBATCH -e job.%j.err
 ##SBATCH --array=1-16
-## path="/hpc2hdd/home/zzhi359/quantumErgotropy/julia_shell/erg_scaling"
 
 
 > resource_usage.log
@@ -23,39 +22,17 @@
 ) >> resource_usage.log &
 
 echo "Program started at: $(date)"
-echo "Received parameters: index=$index, seed=$seed"
-cd ..
+cd /hpc2hdd/home/zzhi359/FibonacciChain.jl
 
 
+for ((j=8; j<=20; j+=2)); do
+  for ((i=1; i<=2000; i++)); do
+      # 生成一个随机种子
+      RANDOM_SEED=($(j+i) * 1000 ) # 通过任务ID来生成种子，确保不同任务之间不重复
 
-CPU_LIMIT=$(nproc)    # 通常为44
-TOTAL_MEM_GB=503      # 503 GB
-PER_TASK_MEM_GB=2    # 每个任务的预计内存使用
-MEM_LIMIT=$(( TOTAL_MEM_GB / PER_TASK_MEM_GB ))  # 251
-
-# 设定最大并发数
-if [[ MEM_LIMIT -lt CPU_LIMIT ]]; then
-    CONCURRENCY=$MEM_LIMIT
-else
-    CONCURRENCY=$CPU_LIMIT
-fi
-
-for ((i=1; i<=2000; i++)); do
-    # 生成一个随机种子
-    RANDOM_SEED=$(( RANDOM + i * 1000 ))  # 通过任务ID来生成种子，确保不同任务之间不重复
-
-    export JULIA_NUM_THREADS=16
-    julia --project=. exm/Bulk_measure/monitored_dynamics.jl 20 $i $RANDOM_SEED
-
-    ((task_counter++))
-    echo "Submitted job $i (concurrent: $task_counter/$CONCURRENCY)"
-
-    # 控制并发任务数量
-    if (( task_counter >= CONCURRENCY )); then
-        wait -n  # 等待任意一个完成的任务
-        ((task_counter--))
-        echo "Job completed, current concurrent: $task_counter"
-    fi
+      sbatch /hpc2hdd/home/zzhi359/FibonacciChain.jl/exm/compute.sh $j $i $RANDOM_SEED
+    
+  done
 done
 
 echo "mission accomplished at: $(date)"
