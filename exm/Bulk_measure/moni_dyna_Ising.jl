281492425a228d5c86fa1eecfd753bb81db247f4
Zhaohui Zhi <2276780832@qq.com>
2025-07-24 02:08:52 +0800

    update

--- exm/Bulk_measure/moni_dyna_Ising.jl
+++ exm/Bulk_measure/moni_dyna_Ising.jl
@@ -3,39 +3,46 @@ using JLD
 using Statistics
 using Random
 
+function total_samples_generate(L::Int64, τ::Float64, index::Int64, seed::Int64, D::Int64=120L)
+    for i in 0:99
+        @show (index+i), (seed+i)
+        samples_generate(L, τ, index+i, seed + i, D)
+    end
+end
+
 function samples_generate(L::Int64, τ::Float64, index::Int64, seed::Int64, D::Int64=120L)
     rng = MersenneTwister(seed)
     
-    st = zeros(length(Fibonacci_basis(L)))
+    st = zeros(length(Fibonacci_basis(L, measure_class=:IsingX)))
     st[1] = 1.0
     
-    @time sample_measured_states, sample, sample_free_energy = Bulkmeasure(L, τ, st, D, rng, true) 
-    
-    halfchain_EE_tlis = [ee(rdm_Fibo(L, collect(1:div(L,2)), j)) for j in sample_measured_states]
+    @time sample_measured_states, sample, sample_free_energy = Bulkmeasure(L, τ, st, D, rng, true, measure_class=:IsingX) 
+
+    halfchain_EE_tlis = [ee(rdm_Fibo(L, collect(1:div(L,2)), j, measure_class = :IsingX)) for j in sample_measured_states]
     final_state = sample_measured_states[end]
-    final_EElis = eelis_Fibo_state(L, final_state)
+    final_EElis = eelis_Fibo_state(L, final_state, measure_class = :IsingX)
 
     
-    save("./exm/data/Bulk_measure/Observable_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(index).jld", "halfchain_EE_tlis", halfchain_EE_tlis, "final_EElis ", final_EElis, "seed", seed, "sample_free_energy", sample_free_energy)
+    save("./exm/data/Bulk_measure/Ising/Observable_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(index).jld", "halfchain_EE_tlis", halfchain_EE_tlis, "final_EElis ", final_EElis, "seed", seed, "sample_free_energy", sample_free_energy)
 
-    save("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(index).jld", "sample", sample, "sample_free_energy", sample_free_energy, "seed", seed)
+    save("exm/data/Bulk_measure/Ising/Samples_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(index).jld", "sample", sample, "sample_free_energy", sample_free_energy, "seed", seed)
     # return sample_measured_states, samples, sample_free_energy
 end
 
 function sample_continue_calculate(L::Int64, τ::Float64, index::Int64, seed::Int64, D::Int64=120L, additional_layers::Int64=15L)
     rng = MersenneTwister(seed)
     
-    sample, sample_free_energy, seed= load("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(index).jld", "sample", "sample_free_energy","seed")
-    st = generate_state(τ, st, sample, true, temp= true) 
-    sample_measured_states, sample, sample_free_energy = Bulkmeasure(L, τ, st[end-1],additional_layers, rng) 
-    halfchain_EE_tlis = [ee(rdm_Fibo(L, collect(1:div(L,2)), j)) for j in sample_measured_states]
+    sample, sample_free_energy, seed= load("exm/data/Bulk_measure/Ising/Samples_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(index).jld", "sample", "sample_free_energy","seed")
+    st = generate_state(τ, st, sample, true, temp= true)
+    sample_measured_states, sample, sample_free_energy = Bulkmeasure(L, τ, st[end-1],additional_layers, rng)
+    halfchain_EE_tlis = [ee(rdm_Fibo(L, collect(1:div(L,2)), j, measure_class = :IsingX)) for j in sample_measured_states]
     final_state = sample_measured_states[end]
-    final_EElis = eelis_Fibo_state(L, final_state)
+    final_EElis = eelis_Fibo_state(L, final_state, measure_class = :IsingX)
 
     
-    save("./exm/data/Bulk_measure/Observable_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D+additional_layers,L))_Samples$(index).jld", "halfchain_EE_tlis", halfchain_EE_tlis, "final_EElis ", final_EElis, "seed", seed, "sample_free_energy", sample_free_energy)
+    save("./exm/data/Bulk_measure/Ising/Observable_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D+additional_layers,L))_Samples$(index).jld", "halfchain_EE_tlis", halfchain_EE_tlis, "final_EElis ", final_EElis, "seed", seed, "sample_free_energy", sample_free_energy)
 
-    save("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D+additional_layers,L))_Samples$(index).jld", "sample", sample, "sample_free_energy", sample_free_energy, "seed", seed)
+    save("exm/data/Bulk_measure/Ising/Samples_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D+additional_layers,L))_Samples$(index).jld", "sample", sample, "sample_free_energy", sample_free_energy, "seed", seed)
 end
 
 function samples_collect(L::Int64, τ::Float64, D::Int64=120L)
@@ -45,13 +52,13 @@ function samples_collect(L::Int64, τ::Float64, D::Int64=120L)
     ensemble_seed = Vector{Int64}(undef, samples_num)
      for i in 1:samples_num
         @show i
-        @time sample, sample_free_energy, seed = load("exm/data/Bulk_measure/Samples_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(i).jld", "sample", "sample_free_energy", "seed")
+        @time sample, sample_free_energy, seed = load("exm/data/Bulk_measure/Ising/Samples_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(i).jld", "sample", "sample_free_energy", "seed")
         ensemble[i] = sample
         ensemble_free_energy[i] = sample_free_energy
         ensemble_seed[i] = seed
     end
 
-    save("exm/data/Bulk_measure/monitored_dynamics_ensemble_L$(L)_τ$(τ)_D$(div(D,L)).jld", "ensemble", ensemble, "ensemble_free_energy", ensemble_free_energy, "ensemble_seed", ensemble_seed)
+    save("exm/data/Bulk_measure/Ising/monitored_dynamics_ensemble_L$(L)_τ$(τ)_D$(div(D,L)).jld", "ensemble", ensemble, "ensemble_free_energy", ensemble_free_energy, "ensemble_seed", ensemble_seed)
 end
 
 
@@ -64,7 +71,7 @@ function Observable_collect(L::Int64, τ::Float64, D::Int64=120L)
 
     for i in 1:samples_num
         @show i
-        halfchain_EE_tlis, final_EElis, seed, sample_free_energy = load("./exm/data/Bulk_measure/Observable_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(i).jld", "halfchain_EE_tlis", "final_EElis ", "seed",  "sample_free_energy")
+        halfchain_EE_tlis, final_EElis, seed, sample_free_energy = load("./exm/data/Bulk_measure/Ising/Observable_monitored_dynamics/L$(L)/τ$(τ)/D$(div(D,L))_Samples$(i).jld", "halfchain_EE_tlis", "final_EElis ", "seed",  "sample_free_energy")
 
         ensemble_EE_dynamics[i, :] = halfchain_EE_tlis
         ensemble_final_EElis[i, :] = final_EElis
@@ -74,11 +81,11 @@ function Observable_collect(L::Int64, τ::Float64, D::Int64=120L)
 
     bulk_meanEElis = mean(ensemble_final_EElis, dims=1)[:]
     average_EE_tlis = mean(ensemble_EE_dynamics, dims=1)[:]
-    ensemble_stderr_EElis = (std(ensemble_final_EElis, dims=1) ./ sqrt(samples_num))[:]
+    bulk_stderr_EElis = (std(ensemble_final_EElis, dims=1) ./ sqrt(samples_num))[:]
     stderr_EE_tlis = (std(ensemble_EE_dynamics, dims=1) ./ sqrt(samples_num))[:]
 
     
-    save("exm/data/Bulk_measure/monitored_EE_FEdynamics_L$(L)_τ$(τ)_D$(div(D,L)).jld", "average_EE_tlis", average_EE_tlis, "stderr_EE_tlis", stderr_EE_tlis, "bulk_meanEElis", bulk_meanEElis, "ensemble_stderr_EElis",ensemble_stderr_EElis, "ensemble_free_energy", ensemble_free_energy, "ensemble_seed", ensemble_seed)
+    save("exm/data/Bulk_measure/Ising/monitored_EE_FEdynamics_L$(L)_τ$(τ)_D$(div(D,L)).jld", "average_EE_tlis", average_EE_tlis, "stderr_EE_tlis", stderr_EE_tlis, "bulk_meanEElis", bulk_meanEElis, "bulk_stderr_EElis",bulk_stderr_EElis, "ensemble_free_energy", ensemble_free_energy, "ensemble_seed", ensemble_seed)
 end
 
 function monitored_dynamics(L::Int64, τ::Float64, D::Int64=20L, window = 5L:D-5)
