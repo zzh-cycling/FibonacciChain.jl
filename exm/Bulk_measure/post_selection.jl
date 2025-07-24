88ec97fab7b14f86c653277d8486bfa553afdf3e
Zhaohui Zhi <2276780832@qq.com>
2025-07-24 21:45:19 +0800

    update

--- exm/Bulk_measure/post_selection.jl
+++ exm/Bulk_measure/post_selection.jl
@@ -17,10 +17,17 @@ function post_selection(L::Int64, τ::Float64, D::Int64, sign::Int64=1)
     return average_EElis, EE_tlis, sample_free_energy
 end
 
-for i in 8:2:20
-    L = i
-    τ = 0.1
-    D = 60L
-    average_EElis, EE_tlis, sample_free_energy = post_selection(L, τ, D, 1)
-    save("exm/data/post_selection1/τ$(τ)/L$(L)_D$(div(D,L)).jld", "average_EElis", average_EElis, "EE_tlis", EE_tlis, "sample_free_energy", sample_free_energy)
+
+
+for τ in τlis[end]
+    for L in 8:2:20
+        D = get_system_params(τ, L)[1]
+        average_EElis, EE_tlis, sample_free_energy = post_selection(L, τ, D, 1)
+        save("exm/data/post_selection1/τ$(τ)/L$(L)_D$(div(D,L)).jld", "average_EElis", average_EElis, "EE_tlis", EE_tlis, "sample_free_energy", sample_free_energy)
+    end
 end
+
+
+L=8
+average_EElis, EE_tlis, sample_free_energy = post_selection(L, 0.6, 20L, 1)
+
