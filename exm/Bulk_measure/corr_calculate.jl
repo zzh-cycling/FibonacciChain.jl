f3ecdb5c55cf585dd4b386a5ac10287a32a8f43a
Zhaohui Zhi <2276780832@qq.com>
2025-07-28 01:21:11 +0800

    up

--- exm/Bulk_measure/corr_calculate.jl
+++ exm/Bulk_measure/corr_calculate.jl
@@ -19,6 +19,8 @@ S1_state = add_reference_qubit!(initial_state, 1, pbc, measure_class=measure_cla
 S2_state = add_reference_qubit!(initial_state, 2, pbc, measure_class=measure_class)
 S1_rdm = disjoint_rdm(BitStr{N, Int}, BitStr{N, Int}, [1], [2], S1_state, pbc; measure_classA=measure_class, measure_classB=measure_class)
 S2_rdm = disjoint_rdm(BitStr{N, Int}, BitStr{N, Int}, [1], [2], S2_state, pbc; measure_classA=measure_class, measure_classB=measure_class)
+S12_rdm = disjoint_rdm(BitStr{N, Int}, BitStr{N, Int}, [1, 2], [1, 2], S1_state, pbc; measure_classA=measure_class, measure_classB=measure_class)
 S1_ee = ee(S1_rdm)
 S2_ee = ee(S2_rdm)
+S12_ee = ee(S12_rdm)
 I = S1_ee + S2_ee- S12_ee
