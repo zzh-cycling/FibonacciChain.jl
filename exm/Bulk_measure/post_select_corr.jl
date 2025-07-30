460a25714fa7734464ea93d89dbd299fe068d8d6
Zhaohui Zhi <2276780832@qq.com>
2025-07-31 00:16:20 +0800

    up

--- exm/Bulk_measure/post_select_corr.jl
+++ exm/Bulk_measure/post_select_corr.jl
@@ -67,10 +67,59 @@ function compute_post_selection(L::Int64, τ::Float64, D::Int64=35L, start_point
     spatial_corr = spatial_correlation(L, final_st, 1, div(L,2), pbc)
     
     timeslice1 = L*start_point
-    temporal_corr_lis = [temporal_correlation(τ, initial_state, sample, div(L,2), timeslice1, j) for j in timeslice1+L:D-3L]
+    temporal_corr_lis = [temporal_correlation(τ, initial_state, sample, div(L,2), timeslice1, j) for j in timeslice1+5:timeslice1+2L]
 
     save("exm/data/Bulk_measure/temporal_corr/L$(L)/τ$(τ)/D$(div(D,L))_ps1.jld", "temporal_corr_lis", temporal_corr_lis, "spatial_corr", spatial_corr)
 end
+
+# function get_system_params_corr(τ)
+#     if τ == log(1 + √2)
+#         D = 35
+#     elseif τ == atanh(0.1)
+#         D = 2500
+#     elseif τ == atanh(0.2)
+#         D = 500
+#     elseif τ == atanh(0.3)
+#         D = 120
+#     elseif τ == atanh(0.4)
+#         D = 100
+#     elseif τ == atanh(0.5)
+#         D=80
+#     elseif τ == atanh(0.6)
+#         D=45
+#     elseif τ == atanh(0.8)
+#         D=25
+#     elseif τ == atanh(0.9) || τ == atanh(0.95)
+#         D=8
+#     elseif τ == atanh(0.999)
+#         D=5
+#     else
+#         D = 5  # Default value for τ=1000.0
+#     end
+#     return D
+# end
+
+# function plot_corr(L_list=collect(8:2:24))
+#     c = cgrad(:blues, length(L_list), categorical=true)
+    
+#     fig = plot(
+#         legend_background_color=nothing,
+#         legend_foreground_color=nothing, 
+#         xlabel=L"\Delta t /L",
+#         ylabel=L"g(0, \Delta t)/g_{space}"
+#     )
+#     # annotate!(fig_monitored_N, [(335, 3.6, text(L"L=", 10, :black))])
+#     for (idx, L) in enumerate(L_list)
+#         D = get_system_params_corr(τ)
+        
+#         temporal_corr_lis, spatial_corr = load("exm/data/Bulk_measure/temporal_corr/L$(L)/τ$(τ)/D$(D)_ps1.jld",  "temporal_corr_lis", "spatial_corr")
+        
+
+#         plot!(fig ,collect(1:length(temporal_corr_lis))./L, temporal_corr_lis ./spatial_corr, label=latexstring("$(L)"), legendtitle=L"L", color=c[idx], linewidth=2)
+#     end
+
+#     return fig
+# end
 
 γlis = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.707, 0.8, 0.9, 0.95, 0.999, 1]
 τlis = atanh.(γlis)
@@ -89,10 +138,10 @@ else
     compute_post_selection(L, τ, D)
 end
 
-# temporal_corr_lis, spatial_corr = load("exm/data/Bulk_measure/temporal_corr/L$(L)/τ$(τ)/D$(div(D,L))_ps1.jld",  "temporal_corr_lis", "spatial_corr")
 
-# plot(collect(1:length(temporal_corr_lis))./L, temporal_corr_lis ./spatial_corr, xlabel=L"\Delta t /L", ylabel=L"g(0, \Delta t)/g_{space}", label=false)
 
+# τ = log(1 + √2)  
+# fig = plot_corr(collect(8:4:12))
 # inds = findall(x-> isapprox(x, 1.0, atol=0.1), temporal_corr_lis ./ spatial_corr)
 # Δt = (collect(1:length(temporal_corr_lis))./L)[inds][end-1]
 # α = log(1+√2)/π/Δt
