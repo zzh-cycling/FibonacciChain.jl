a2edd3cd5b365968f8040551c2ad287953e89af3
Zhaohui Zhi <2276780832@qq.com>
2025-08-15 23:15:51 +0800

    add Documentation

--- exm/Bulk_measure/ps_prob.jl
+++ exm/Bulk_measure/ps_prob.jl
@@ -1,7 +1,7 @@
 using FibonacciChain
 using JLD
 using Random
-# include("../FitEntEntScal.jl")
+include("../FitEntEntScal.jl")
 
 binary_distribution(p, rng) = rand(rng) < p ? 1 : 0
 function ps_prob_evolution(L::Int64, τ::Float64,  D::Int, prob::Float64; seed::Int=100, temp::Bool=false)
@@ -30,7 +30,7 @@ else
     seed=parse(Int64, ARGS[1])
     stlis = [ps_prob_evolution(L, τ, D, p, seed=seed) for p in problis]
     eelis = [anyon_eelis(L, st) for st in stlis]
-    save("exm/Bulk_measure/data/ps_prob_evolution/L$(L)_D$(div(D,L))_τ$(τ)_sample_$(seed).jld", "seed", seed, "eelis", eelis, "problis", problis)
+    save("exm/data/Bulk_measure/ps_prob_evolution/L$(L)_D$(div(D,L))_τ$(τ)_sample_$(seed).jld", "seed", seed, "eelis", eelis, "problis", problis)
 end
 
 
@@ -43,13 +43,3 @@ end
 # end
 # save("exm/Bulk_measure/data/ps_prob_sample_$(i).jld", "seed", collect(1:1000))
 # prob_eelis ./= num_samples
-
-# centlis = zeros(length(problis))
-# centerrlis = zeros(length(problis))
-
-# for (i, ee) in enumerate(eelis)
-#     cent, fig = fitCCEntEntScal(ee, mincut=2, pbc=true)
-#     centlis[i] = cent[1]
-#     centerrlis[i] = cent[2]
-#     savefig(fig, "./exm/fig/ps_prob_evocc_L$(L)_D$(D)_p$(problis[i]).pdf")    
-# end
