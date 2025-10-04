08dc2c5946590adadee9de9f728032dd3490320c
Zhaohui Zhi <2276780832@qq.com>
2025-10-04 21:04:52 +0800

    up_process

--- exm/Bulk_measure/ps_prob.jl
+++ exm/Bulk_measure/ps_prob.jl
@@ -2,7 +2,7 @@ using FibonacciChain
 using JLD
 using Random
 using Statistics
-# include("../FitEntEntScal.jl")
+
 
 binary_distribution(p, rng) = rand(rng) < p ? 1 : 0
 function ps_prob_evolution(L::Int64, τ::Float64, D::Int; seed::Int=100)
@@ -64,6 +64,50 @@ function get_system_params(τ, L)
     inds = collect(1:step:D)
     avg_range = start:D-5
     return D, inds, avg_range
+end
+
+function process_ps_prob_evolution(L, τ, D)
+    # Load the data
+    samplelis = collect(1:1000)
+    problis = collect(0.1:0.1:0.9)
+    centlis = []
+    seedlis = zeros(Int64, length(samplelis))
+    # Process the data
+    ee_problis = zeros(L-1, length(problis))
+    for (i, sample) in enumerate(samplelis)
+        ee_plis, seed = load("exm/data/Bulk_measure/ps_prob_evolution/L$(L)/τ$(τ)/L$(L)_D$(div(D,L))_τ$(τ)_sample$(sample).jld", "ee_plis", "seed")
+        ee_problis += hcat(ee_plis...)
+        # cent_problis[i, j] = 
+        seedlis[i] = seed
+    end
+
+    ee_problis ./= length(samplelis)
+    
+
+    for i in eachindex(problis)
+        push!(centlis, fitCCEntEntScal(vec(ee_problis[:, i]), mincut=2, pbc=true)[1])
+    end
+
+    save("exm/data/Bulk_measure/ps_prob_evolution/L$(L)/τ$(τ)/L$(L)_D$(div(D,L))_τ$(τ)_cent.jld", "centlis", centlis, "seedlis", seedlis)
+    return centlis, seedlis
+end
+
+function process_data()
+    Llis = collect(8:2:16)
+    problis = collect(0.1:0.1:0.9)
+    ixs = [1, 3, 4, 7, 9, 10, 12]
+    for (inds, τ) in enumerate(τlis[ixs])
+        cent_Lplis = zeros(length(Llis), length(problis))
+        cent_stderrlis = zeros(length(Llis), length(problis))
+        for (id, L) in enumerate(Llis)
+            D, _, _ = get_system_params(τ, L)
+            @show (L, τ)
+            centlis= load("exm/data/Bulk_measure/ps_prob_evolution/L$(L)/τ$(τ)/L$(L)_D$(div(D,L))_τ$(τ)_cent.jld", "centlis")
+            cent_Lplis[id, :] = [i[1] for i in centlis]
+            cent_stderrlis[id, :] = [i[2] for i in centlis]
+        end
+        save("exm/data/Bulk_measure/ps_prob_evolution/centlis_L$(Llis[1])$(Llis[end])_τ$(τ).jld", "cent_Lplis", cent_Lplis, "cent_stderrlis", cent_stderrlis)
+    end
 end
 
 γlis = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.707, 0.8, 0.9, 0.95, 0.999, 1]
