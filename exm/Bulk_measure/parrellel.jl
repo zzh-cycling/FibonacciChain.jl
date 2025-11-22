97cf942feff4f33075366b0ed76a4b86c2ea29aa
Zhaohui Zhi <2276780832@qq.com>
2025-11-23 01:55:03 +0800

    up

--- exm/Bulk_measure/parrellel.jl
+++ exm/Bulk_measure/parrellel.jl
@@ -5,6 +5,71 @@ using Distributed
 @everywhere using Random
 @everywhere using LinearAlgebra
 
+println("requested workers: ", nworkers())
+println("total procs:       ", nprocs())
+@everywhere println("host: ", gethostname(), "  pid: ", getpid())
+
+@everywhere function get_δtL(τ, L)
+    if L == 6
+        table = Dict(
+                atanh(0.1)  => (collect(4:150)),
+                atanh(0.2)  => (collect(0:20)),
+                atanh(0.3)  => (collect(0:20)),
+                atanh(0.4)  => (collect(0:20)),)
+        δtlis = get(table, τ, collect(0:10))
+    elseif L == 8
+        if τ ∈ τlis[1:4]
+            δtlis = collect(0:20)
+        else
+            δtlis = collect(0:10)
+        end
+    elseif L == 10
+        table = Dict(
+                atanh(0.1)  => vcat([0], collect(20:40)),
+                atanh(0.2)  => vcat([0],collect(10:20)),
+                atanh(0.3)  => (collect(0:20)),
+                atanh(0.4)  => (collect(0:20)),)
+        δtlis = get(table, τ, collect(0:10))
+    elseif L == 12
+        table = Dict(
+                atanh(0.1)  => vcat([0], collect(80:100)),
+                atanh(0.2)  => vcat([0], collect(30:40)),
+                atanh(0.3)  => vcat([0], collect(15:25)),
+                atanh(0.4)  => vcat([0], collect(15:25)),)
+        δtlis = get(table, τ, collect(0:10))
+    elseif L == 14
+        table = Dict(
+                atanh(0.1)  => vcat([0],collect(28:35)),
+                atanh(0.2)  => vcat([0],collect(14:20)),
+                atanh(0.3)  => vcat([0],collect(7:14)),
+                atanh(0.4)  => vcat([0],collect(5:10)),
+                atanh(0.5)  => vcat([0],collect(5:10)),)
+        δtlis = get(table, τ, collect(0:8))
+    elseif L == 16
+        table = Dict(
+                atanh(0.1)  => vcat([0],collect(32:42)),
+                atanh(0.2)  => vcat([0],collect(16:24)),
+                atanh(0.3)  => vcat([0],collect(8:16)),
+                atanh(0.4)  => vcat([0],collect(4:12)),
+                atanh(0.5)  => vcat([0],collect(4:12)),)
+        δtlis = get(table, τ, collect(0:8))
+    elseif L == 18
+        table = Dict(
+                atanh(0.1)  => vcat([0],collect(140:150)),
+                atanh(0.2)  => vcat([0],collect(55:70)),
+                atanh(0.3)  => vcat([0],collect(30:40)),
+                atanh(0.4)  => vcat([0],collect(25:35)),
+                atanh(0.5)  => vcat([0],collect(18:25)),
+                atanh(0.6)  => vcat([0],collect(15:22)),
+                atanh(1/√2)  => vcat([0],collect(10:18)),
+                atanh(0.8)  => vcat([0],collect(10:18)),)
+        δtlis = get(table, τ, collect(0:8))
+    else
+        δtlis = collect(1:10)
+    end
+    return  δtlis
+end
+
 @everywhere function get_system_params(τ, L)
     cfg = Dict(
         atanh(0.1)  => (2500L, 1000, 1500L),
@@ -291,7 +356,7 @@ else
         # batch computation for multiple δt values
         start_seed = parse(Int, ARGS[4])
         end_seed = parse(Int, ARGS[5])
-        δt_list = (L == 16) ? collect(0:10) : collect(0:20)
+        δt_list = get_δt_list(L, τ)
         
         println("Batch computation mode")
         compute_parallel_multiple_dt(L, τ, start_seed:end_seed, D, δt_list)
